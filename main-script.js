74951e069eb2279797556748aed5ab556ee65651
Your Name <syngo67@gmail.com>
2017-05-25 11:54:08 +0530

    first commit

---
+++ main-script.js
@@ -0,0 +1,24 @@
+
+// NAVBAR  SCROLLSPY
+
+$(window).scroll(function () {
+    if ($ (document).scrollTop() > 300){
+        $('nav').addClass('Scrolled-nav')
+    }
+    else{
+        $('nav').removeClass('Scrolled-nav')
+    }
+
+});
+ // admin info flip
+$(document).ready(function(){
+    //  the hover to be triggered when the element is tapped on a touch device
+    $('.hover').hover(function(){
+        $(this).addClass('flip');
+    },function(){
+        $(this).removeClass('flip');
+    });
+});
+// nav-active
+// $('#home .item').eq(randSlide).addClass('active');
+#Bernabeu.com
